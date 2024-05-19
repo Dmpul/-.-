7a3aa21439351c28bab4e350f12c54d8c5a58184
Дмитрий <dmpul@mail.ru>
2024-05-19 17:15:18 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,8 @@
+name = input('Введите Ваше имя: ')
+print('Доброго времени суток, ', name)
+my_string = input('Введите текущее время года: ')
+print(len(my_string))
+print(my_string.upper())# Выведите строку my_string в верхнем регистре.
+print(my_string.lower())# Выведите строку my_string в нижнем регистре.
+print('Введите текущее время года'.replace(' ',''))# Измените строку my_string, удалив все пробелы.
+print(my_string[0])# Выведите первый символ строки my_string.
